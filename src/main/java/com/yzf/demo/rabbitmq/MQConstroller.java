cdfc51e36958bc15c0bf3a88545b034e80152e13
miniyzf <mini.yzf@qq.com>
2019-12-12 18:21:35 +0800

    web 端通过 websocket 连接 RabbitMQ，mq.html

---
+++ src/main/java/com/yzf/demo/rabbitmq/MQConstroller.java
@@ -0,0 +1,41 @@
+package com.yzf.demo.rabbitmq;
+
+import com.yzf.demo.rabbitmq.topic.TopicProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.websocket.server.PathParam;
+
+/**
+ * MQConstroller
+ *
+ * @author Administrator
+ * @date 2019/12/12
+ */
+@Controller
+public class MQConstroller {
+    @GetMapping("/mq")
+    public ModelAndView goMq(){
+        ModelAndView mav = new ModelAndView("mq");
+        return mav;
+    }
+
+    @Autowired
+    private TopicProducer topicProducer;
+
+    @RequestMapping("/exchange/topic")
+    public @ResponseBody String sendTopicMessage(@PathParam("message")String message){
+        topicProducer.sendRoutingKeyOne(message);
+        return message;
+    }
+
+    @RequestMapping("/exchange/topic")
+    public @ResponseBody String sendTopicMsg(@PathParam("msg")String msg){
+        topicProducer.sendRoutingKeyTwo(msg);
+        return msg;
+    }
+}
